d6b8269d575fffc8b3575b001f15ad2574ba72e4
avireale13 <114672168+avireale13@users.noreply.github.com>
2023-10-13 11:04:13 -0500

    Adding timer

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,13 +9,12 @@ public class Main {
         int n = 1000;
         Point[] points = new Point[n];
         for (int i = 0; i < n; i++) {
-            double x = random.nextDouble(10000);
-            double y = random.nextDouble(10000);
+            double x = random.nextDouble() * 10000;
+            double y = random.nextDouble() * 10000;
             points[i] = new Point(x, y);
         }
 
         for (int i = 0; i > -1; i++) {
-
             System.out.println("What algorithm would you like to use?\n" +
                     "1. Brute Force\n" +
                     "2. Divide and Conquer\n" +
@@ -25,57 +23,61 @@ public class Main {
                     "5. End");
             algorithm = scan.nextInt();
 
+            long startTime = System.nanoTime(); // Start the timer
+
             switch (algorithm) {
                 case 1:
                     int[] closestPair1 = BruteForce.bruteForce(points);
-
-                    System.out.println("Closest Pair Indices: (" + closestPair1[0] + ", " + closestPair1[1] + ")");
-
-                    Point point1 = points[closestPair1[0]];
-                    Point point2 = points[closestPair1[1]];
-
-                    System.out.println("List of Points:");
-                    for (int j = 0; j < n; j++) {
-                        System.out.println("Point " + (j + 1) + ": (" + points[j].x + ", " + points[j].y + ")");
-                    }
-
-                    System.out.println("Closest Pair Distance: " + getDistance(point1, point2));
+                    displayResult(points, closestPair1);
                     break;
 
                 case 2:
                     int[] closestPair2 = DivideAndConquer.divideAndConquer(points);
-
-                    System.out.println("Closest Pair Indices: (" + closestPair2[0] + ", " + closestPair2[1] + ")");
-
-                    Point point3 = points[closestPair2[0]];
-                    Point point4 = points[closestPair2[1]];
-
-                    System.out.println("List of Points:");
-                    for (int j = 0; j < n; j++) {
-                        System.out.println("Point " + (j + 1) + ": (" + points[j].x + ", " + points[j].y + ")");
-                    }
-
-                    System.out.println("Closest Pair Distance: " + getDistance(point3, point4));
+                    displayResult(points, closestPair2);
                     break;
 
                 case 3:
+                    // Implement Decrease and Conquer
                     break;
+
                 case 4:
+                    // Implement Random
                     break;
+
                 case 5:
-                    i=-3;
+                    i = -3;
+                    break;
 
                 default:
                     return;
             }
-            System.out.println();
+
+            long endTime = System.nanoTime(); // Stop the timer
+            double elapsedTime = (endTime - startTime) / 1e6; // Convert to milliseconds
+            System.out.println("Time taken: " + elapsedTime + " ms\n");
         }
     }
+
+    public static void displayResult(Point[] points, int[] closestPairIndices) {
+        System.out.println("Closest Pair Indices: (" + closestPairIndices[0] + ", " + closestPairIndices[1] + ")");
+
+        Point point1 = points[closestPairIndices[0]];
+        Point point2 = points[closestPairIndices[1]];
+
+        System.out.println("List of Points:");
+        for (int j = 0; j < points.length; j++) {
+            System.out.println("Point " + (j + 1) + ": (" + points[j].x + ", " + points[j].y + ")");
+        }
+
+        System.out.println("Closest Pair Distance: " + getDistance(point1, point2));
+    }
+
     public static double getDistance(Point p1, Point p2) {
         double dx = p1.x - p2.x;
         double dy = p1.y - p2.y;
         return Math.sqrt(dx * dx + dy * dy);
     }
 }
+
 
 
