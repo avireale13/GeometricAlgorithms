3b2704189fe09c1a04bd107b89dcc71412662964
Bryann0000 <135722671+Bryann0000@users.noreply.github.com>
2023-10-17 21:20:23 -0400

    Update Randomized.java
    Added comments to a few extra lines

--- src/Randomized.java
+++ src/Randomized.java
@@ -3,7 +3,7 @@ import java.util.*;
   // Will often fail to select the true closest pair for two possible reasons: 
     // 1) Point values can repeat (preventing the smallest pair from being compared)
       // This can be resolved by having non-repeating random generators or an array with n values randomized
-    // 2) The same points to be compared giving a false closest pair of 0.
+    // 2) The same points will be compared giving a false closest pair of 0.
       // This can be resolved by having the if loop for distance checking only occur if (&& rand1 != rand2)
     // For the purpose of this program, these solutions will not be implemented
 
@@ -15,10 +15,10 @@ public class Randomized {
 
         Random rand = new Random();
         int rand1, rand2;
-        for (int i = 0; i < n ; i++) {
+        for (int i = 0; i < n; i++) { // Run for loops until it matches n, so if n is 1000, it runs from 0-999 (or 1000)
           {
-            rand1 = rand.nextInt(n-1);
-            rand2 = rand.nextInt(n-2);
+            rand1 = rand.nextInt(n-1); // Generates a random value between 0 to n-1 (For N = 1000, it would generate 0-999)
+            rand2 = rand.nextInt(n-1); // Generates a random value between 0 to n-1 (For N = 1000, it would generate 0-999)
             double dx = points[rand1].x - points[rand2].x;
             double dy = points[rand1].y - points[rand2].y;
             double distance = Math.sqrt(dx * dx + dy * dy);
