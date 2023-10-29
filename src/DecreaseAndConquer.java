7ecde9436c580f4b133ac1f588ed9f8932a3b667
DejaHintzen <115024992+DejaHintzen@users.noreply.github.com>
2023-10-29 16:49:10 -0400

    Update DecreaseAndConquer.java

--- src/DecreaseAndConquer.java
+++ src/DecreaseAndConquer.java
@@ -1,39 +1,84 @@
 import java.util.*;
+
 public class DecreaseAndConquer {
     public static int[] decreaseAndConquer(Point[] points) {
+        if (points == null || points.length < 2) {
+            return null; //Returns if there are not enough points tp form a pair
+        }
 
-        Arrays.sort(points, Comparator.comparingDouble(point -> point.x));
+        Random random = new Random();
+        for (Point point : points) {
+            point.x += random.nextDouble() * 0.000001; //Random offset to x-coordinate
+            point.y += random.nextDouble() * 0.000001; //Random offset to y-coordinate
+        }
 
-        return closestPairRecursive(points, 0, points.length - 1);
+
+        Arrays.sort(points, Comparator.comparingDouble(p -> p.x)); //Sorts array of points by x-coordinate.
+        int[] closestPairIndices = new int[2];
+        closestPair(points, 0, points.length - 1, closestPairIndices);
+        return closestPairIndices;
     }
-
-    public static int[] closestPairRecursive(Point[] points, int left, int right) {
-        if (left >= right) {
-            return new int[]{-1, -1}; // No valid pair
+    private static double closestPair(Point[] points, int left, int right, int[] closestPairIndices) {
+        if (left == right) {
+            return Double.MAX_VALUE; //Returns if there are no points to compare
+        } else if (left + 1 == right) {
+            return getDistance(points[left], points[right]); //Returns 2 points to compare
         }
 
-        if (left + 1 == right) {
-            return new int[]{left, right}; // Only two points
-        }
+        int middle = (left + right) / 2;
+        int[] leftPair = new int[2];
+        int[] rightPair = new int[2];
 
+        double lDistance = closestPair(points, left, middle, leftPair);
+        double rDistance = closestPair(points, middle + 1, right, rightPair);
+        double minDistance = Math.min(lDistance, rDistance);
 
-        int mid = (left + right) / 2;
+        double splitDistance = closestSplitPair(points, left, middle, right, minDistance, closestPairIndices);
 
-        int[] lClosest = closestPairRecursive(points, left, mid);
-        int[] rClosest = closestPairRecursive(points, mid + 1, right);
-
-        double leftDistance = dist(points[lClosest[0]], points[lClosest[1]]);
-        double rightDistance = dist(points[rClosest[0]], points[rClosest[1]]);
-
-        if (leftDistance < rightDistance) {
-            return lClosest;
+        if (splitDistance < minDistance) {
+            return splitDistance;
+        } else if (lDistance < rDistance) {
+            closestPairIndices[0] = leftPair[0];
+            closestPairIndices[1] = leftPair[1];
+            return lDistance;
         } else {
-            return rClosest;
+            closestPairIndices[0] = rightPair[0];
+            closestPairIndices[1] = rightPair[1];
+            return rDistance;
         }
     }
-    public static double dist(Point point1, Point point2) {
-        double dx = point1.x - point2.x;
-        double dy = point1.y - point2.y;
+
+    private static double closestSplitPair(Point[] points, int left, int mid, int right, double minDistance, int[] closestPairIndices) {
+        Point[] strip = new Point[right - left + 1]; //In order to find the closest split pair, the points are sorted bt their y-coordinates in the split range
+        int stripSize = 0;
+        double splitDistance = minDistance;
+
+        for (int i = left; i <= right; i++) {
+            if (Math.abs(points[i].x - points[mid].x) < minDistance) {
+                strip[stripSize] = points[i];
+                stripSize++;
+            }
+        }
+
+        Arrays.sort(strip, 0, stripSize, Comparator.comparingDouble(p -> p.y));
+
+        for (int i = 0; i < stripSize; i++) {
+            for (int j = i + 1; j < stripSize && strip[j].y - strip[i].y < splitDistance; j++) {
+                double distance = getDistance(strip[i], strip[j]);
+                if (distance < splitDistance) {
+                    splitDistance = distance;
+                    closestPairIndices[0] = Arrays.asList(points).indexOf(strip[i]);
+                    closestPairIndices[1] = Arrays.asList(points).indexOf(strip[j]);
+                }
+            }
+        }
+
+        return splitDistance;
+    }
+
+    private static double getDistance(Point p1, Point p2) {
+        double dx = p1.x - p2.x;
+        double dy = p1.y - p2.y;
         return Math.sqrt(dx * dx + dy * dy);
     }
 }
